c39ffb720e62f44a98e4f3bb4a8ef46793d8d550
rui-watanabe <wataru610806@gmail.com>
2020-08-02 16:06:48 +0900

    add CartWidget comonent clicked cart summary page test

--- src/shared/CartWidget/CartWidget.spec.tsx
+++ src/shared/CartWidget/CartWidget.spec.tsx
@@ -1,5 +1,12 @@
+import React from "react"
 import { CartWidget } from './CartWidget'
+import { fireEvent } from "@testing-library/react"
 
 describe("CartWidget", () => {
-  it.todo("renders correctly")
+  it.todo("shows the amount of products in the cart")
+  it("navigates to cart summary page on click", () => {
+      const { getByRole, history } = renderWithRouter(() => (<CartWidget />))
+      fireEvent.click(getByRole("link"))
+      expect(history.location.pathname).toEqual("/cart")
+    })
 })
